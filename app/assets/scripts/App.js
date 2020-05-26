ad7532ceb9a97d150420474e55f6c0b2ab3f75bf
asaphb <asaphbinene@outlook.com>
2020-05-26 04:58:14 +0200

    comletion of scrolling and reveal feaure

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
-import '../styles/styles.css' //webpack understand only javascript
-import MobileMenu from './modules/katbinMobileNav'
+import '../styles/styles.css'; //webpack understand only javascript
+import MobileMenu from './modules/katbinMobileNav';
+import RevealOnScroll from './modules/katbinScrolling';
+
+new RevealOnScroll(document.querySelectorAll(".feature-item", 65));
+
+new RevealOnScroll(document.querySelectorAll(".testmonial", 50));
 
 let mobileMenu = new MobileMenu();
 
