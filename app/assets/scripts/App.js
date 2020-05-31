fa02d69646b3e62826553caca476b7946211d330
asaphb <asaphbinene@outlook.com>
2020-05-31 05:32:09 +0200

    completed sticking scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css'; //webpack understand only javascript
 import MobileMenu from './modules/katbinMobileNav';
 import RevealOnScroll from './modules/katbinScrolling';
+import StickingHeader from './modules/sticking-head';
+
+let stickingHeader = new StickingHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item", 65));
 
