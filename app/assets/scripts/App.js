3ab010a667ce8f5b82ecc0655be839c0b8d0ef6c
asaphb <asaphbinene@outlook.com>
2020-06-02 03:27:51 +0200

    completed the modal task

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,14 +3,26 @@ import MobileMenu from './modules/katbinMobileNav';
 import RevealOnScroll from './modules/katbinScrolling';
 import StickingHeader from './modules/sticking-head';
 
-let stickingHeader = new StickingHeader();
 
-new RevealOnScroll(document.querySelectorAll(".feature-item", 65));
+new StickingHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item", 75));
+new RevealOnScroll(document.querySelectorAll(".testmonial", 60));
+new MobileMenu();
+let modal;
 
-new RevealOnScroll(document.querySelectorAll(".testmonial", 50));
-
-let mobileMenu = new MobileMenu();
-
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        if(typeof modal == "undefined"){
+            import('./modules/katbinModal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem."));
+        }else{
+            modal.openTheModal();
+        }
+    })
+})
 if(module.hot) {
     module.hot.accept()
 }
