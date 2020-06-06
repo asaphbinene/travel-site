aab78923ef7fd1e03a004696487346c3e62b58e7
asaphb <asaphbinene@outlook.com>
2020-06-06 02:58:11 +0200

    comfirming the completion of the building process

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,13 @@
  * The first line give instruction to export the configuration
  * The second line direct to the first instruction that need to be consider to run this project by webpack
  */
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const katbinMiniCssExtraPlgin = require('mini-css-extract-plugin')
+const katbinHtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
+
 const katbinPCssPlgins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -13,16 +19,49 @@ const katbinPCssPlgins = [
     require('autoprefixer')
 ]
 
-module.exports = {
+class RunAfterCompile{
+    apply(compiler){
+        compiler.hooks.done.tap('copy images', function(){
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+katbinCssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader', {loader: 'postcss-loader', options:{plugins: katbinPCssPlgins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file){
+    return file.endsWith('.html')
+}).map(function(page){
+    return new katbinHtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    plugins: pages,
+    module: {
+        rules: [
+            katbinCssConfig
+        ]
+    }
+}
+
+if (currentTask == 'dev'){
+    katbinCssConfig.use.unshif('style-loader')
+    config.output= {
         /**
          * The above and the bellow instruction control name and location of the main.js and name and folder
          */
         filename: 'ktbin-bundled.js',
         path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
+    }
+
+    config.devServer = {
         before: function(app, server){
             server._watch('./app/**/*.html')
         },
@@ -30,14 +69,43 @@ module.exports = {
         hot: true,
         port: 3000,
         host:'0.0.0.0'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options:{plugins: katbinPCssPlgins}}]
-            }
-        ]
     }
+
+    config.mode = 'development'
 }
+
+if (currentTask == 'build'){
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }     
+        }
+    })
+    katbinCssConfig.use.unshift(katbinMiniCssExtraPlgin.loader)
+    katbinPCssPlgins.push(require('cssnano'))
+    config.output= {
+        /**
+         * The above and the bellow instruction control name and location of the main.js and name and folder
+         */
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    }
+
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new katbinMiniCssExtraPlgin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+    )
+}
+
+module.exports = config
