ce6aa925f5ce0bec407f97152fc73874e321d662
asaphb <asaphbinene@outlook.com>
2020-06-09 23:07:32 +0200

    trying to fix webpack config pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,96 @@
-/**
- * This file is main config of all automation as assist manager all package use to build this project more easily and organise also to make it easy to maintain 
- * The first line give instruction to export the configuration
- * The second line direct to the first instruction that need to be consider to run this project by webpack
- */
 const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const katbinMiniCssExtraPlgin = require('mini-css-extract-plugin')
-const katbinHtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const fse = require('fs-extra')
 
-const katbinPCssPlgins = [
-    require('postcss-import'),
-    require('postcss-mixins'),
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('postcss-hexrgba'),
-    require('autoprefixer')
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('postcss-hexrgba'),
+  require('autoprefixer')
 ]
 
-class RunAfterCompile{
-    apply(compiler){
-        compiler.hooks.done.tap('copy images', function(){
-            fse.copySync('./app/assets/images', './docs/assets/images')
-        })
-    }
-}
-
-let katbinCssConfig = {
-    test: /\.css$/i,
-    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: katbinPCssPlgins}}]
-}
-
-let pages = fse.readdirSync('./app').filter(function(file){
-    return file.endsWith('.html')
-}).map(function(page){
-    return new katbinHtmlWebpackPlugin({
-        filename: page,
-        template: `./app/${page}`
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy images', function() {
+      fse.copySync('./app/assets/images', './docs/assets/images')
     })
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+  return file.endsWith('.html')
+}).map(function(page) {
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+  })
 })
 
 let config = {
-    entry: './app/assets/scripts/App.js',
-    plugins: pages,
-    module: {
-        rules: [
-            katbinCssConfig,
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env']
-                    }
-                }
-            }
-
-        ]
-    }
+  entry: './app/assets/scripts/App.js',
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig,
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
 
-if (currentTask == 'dev'){
-    katbinCssConfig.use.unshif('style-loader')
-    config.output= {
-        /**
-         * The above and the bellow instruction control name and location of the main.js and name and folder
-         */
-        filename: 'ktbin-bundled.js',
-        path: path.resolve(__dirname, 'app')
-    }
-
-    config.devServer = {
-        before: function(app, server){
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 3000,
-        host:'0.0.0.0'
-    }
-
-    config.mode = 'development'
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
+    filename: 'ktbin-bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+  config.devServer = {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+  config.mode = 'development'
 }
 
-if (currentTask == 'build'){
-    katbinCssConfig.use.unshift(katbinMiniCssExtraPlgin.loader)
-    katbinPCssPlgins.push(require('cssnano'))
-    config.output= {
-        /**
-         * The above and the bellow instruction control name and location of the main.js and name and folder
-         */
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'docs')
-    }
-
-    config.mode = 'production'
-    config.optimization = {
-        splitChunks: {chunks: 'all'}
-    }
-
-    config.plugins.push(
-        new CleanWebpackPlugin(), 
-        new katbinMiniCssExtraPlgin({filename: 'styles.[chunkhash].css'}),
-        new RunAfterCompile()
-    )
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require('cssnano'))
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'docs')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  }
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()  
+  )
 }
 
 module.exports = config
